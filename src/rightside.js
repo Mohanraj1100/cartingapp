38eee7fbf23c6dbd3855a33af92ce78e8ccba1d7
Mohanraj1100 <mohanraj.anbuselvam@tringapps.com>
2023-03-14 23:39:16 +0530

    initial

---
+++ src/rightside.js
@@ -0,0 +1,21 @@
+import './rightside.css';
+const MainContent = (props) => {
+
+
+    return (
+        <>
+            <div className='image-container'>
+                {props.products.map(shoe =>
+                    <div key={shoe.id} className='image'>
+                        <img src={shoe.image} alt={shoe.alt} />
+                        <h5>{shoe.rate}</h5>
+                        <h4>{shoe.brand}</h4>
+                        <button className='cart-button' onClick={() => props.addTocart(shoe)}>{props.btn(shoe)?"Remove from Cart":"Add to Cart"}</button>
+                    </div>
+                )}
+            </div>
+        </>
+    )
+}
+
+export default MainContent;
